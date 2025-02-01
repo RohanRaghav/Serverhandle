cf0c6bf0c7827d98574e84d9745fc0c6df02f4eb
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2025-02-01 11:28:48 +0530

    mongo

--- server.js
+++ server.js
@@ -37,12 +37,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// MongoDB Connection
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error.message));
-
 
 // Cloudinary Configuration
 cloudinary.config({
